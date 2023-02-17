1d55cc2776985d86439a9453955a2ff01f7cb978
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-02-17 19:14:43 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import './style.css'
+
+const app = document.querySelector('#app')!
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')!
+const step = 3
+
+const state = {
+  hue: 0,
+  stepX: step,
+  stepY: step,
+  directionX: 'left',
+  directionY: 'down',
+  height: app.clientHeight,
+  width: app.clientWidth,
+  image: new Image(),
+  imageX: 0,
+  imageY: 0
+}
+
+state.height = app.clientHeight
+state.width = app.clientWidth
+canvas.height = state.height
+canvas.width = state.width
+app.appendChild(canvas)
+
+function render() {
+  ctx.globalCompositeOperation = 'source-over'
+  ctx.fillRect(0, 0, state.width, state.height)
+  ctx.globalCompositeOperation = 'source-over'
+  ctx.fillStyle = `hsla(${state.hue}, 50%, 45%, 1)`
+  ctx.fillRect(0, 0, state.width, state.height)
+  ctx.globalCompositeOperation = 'destination-in'
+  ctx.drawImage(
+    state.image,
+    state.imageX,
+    state.imageY,
+    state.image.width,
+    state.image.height
+  )
+
+  updateDirection()
+  updateHue()
+
+  requestAnimationFrame(() => render())
+}
+
+function updateDirection() {
+  if (state.directionX == 'left') {
+    if (state.imageX >= state.width - state.image.width) {
+      state.directionX = 'right'
+      state.imageX = state.imageX - state.stepX
+    } else {
+      state.imageX = state.imageX + state.stepX
+    }
+  } else {
+    if (state.imageX <= 0) {
+      state.directionX = 'left'
+      state.imageX = state.imageX + state.stepX
+    } else {
+      state.imageX = state.imageX - state.stepX
+    }
+  }
+
+  if (state.directionY == 'down') {
+    if (state.imageY >= state.height - state.image.height) {
+      state.directionY = 'up'
+      state.imageY = state.imageY - state.stepY
+    } else {
+      state.imageY = state.imageY + state.stepY
+    }
+  } else {
+    if (state.imageY <= 0) {
+      state.directionY = 'down'
+      state.imageY = state.imageY + state.stepY
+    } else {
+      state.imageY = state.imageY - state.stepY
+    }
+  }
+}
+
+function updateHue() {
+  state.hue = (state.hue + 0.5) % 360
+}
+
+function onResize() {
+  state.width = canvas.width = window.innerWidth
+  state.height = canvas.height = window.innerHeight
+}
+
+async function loadImage() {
+  new Promise<void>((resolve, reject) => {
+    const image = new Image()
+    image.src = './dvd.png'
+
+    image.onload = () => {
+      state.imageX = Math.floor(Math.random() * (state.width - state.image.width))
+      state.imageY = Math.floor(Math.random() * (state.height - state.image.height))
+      resolve()
+    }
+
+    image.onerror = (err) => {
+      reject(err)
+    }
+
+    state.image = image
+  })
+}
+
+loadImage()
+  .then(() => {
+    window.addEventListener('resize', () => onResize())
+    onResize()
+    render()
+  })
+  .catch(console.log)
